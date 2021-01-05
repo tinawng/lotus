607fe260f174c3c59da870ffd09390c57d42d70e
Tina Wang <tina@tina.cafe>
2021-01-05 18:00:08 +0100

    🚧 Working on landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,32 @@
 <template>
   <div class="container">
-    <section style="margin-left: -11vw">
-      <Logo
-        width="38vw"
-        height="38vw"
-        color="brand-gray"
-        border_width="1.2vw"
-      />
-    </section>
+    <Logo
+      class="absolute z-0"
+      style="left: -22vh"
+      width="78vh"
+      height="78vh"
+      color="brand-gray"
+      border_width="1.2vw"
+    />
 
-    <section class="aa">
+    <section class="w-1/2 z-10 ml-auto">
       <div class="flex-vertical-center-x">
         <Logo width="10rem" height="10rem" />
         <div class="title mt-8">LoTUS</div>
-        <h3 class="mt-2">audio comparing tool</h3>
+        <h3 class="mt-2">audio comparison tool</h3>
       </div>
 
       <form
-        id="app"
-        @submit="checkForm"
-        action="https://vuejs.org/"
-        method="post"
         class="w-2/3"
+        :action="backend_url + '/auth/login'"
+        method="POST"
+        v-on:submit.prevent="onSubmit"
       >
         <p>
-          <input
-            id="login"
-            v-model="login"
-            type="text"
-            name="login"
-            placeholder="email"
-          />
+          <input v-model="login" type="text" name="login" placeholder="email" />
         </p>
         <p>
           <input
-            id="password"
             v-model="password"
             type="password"
             name="password"
@@ -46,47 +38,56 @@
           <input class="button-square" type="submit" value=">" />
         </p>
       </form>
-      <!-- <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div> -->
     </section>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    backend_url() {
+      return process.env.BACKEND_URL;
+    },
+  },
   data: () => ({
     login: "",
+    password: "",
+    err: false,
   }),
+
+  methods: {
+    async onSubmit() {
+      try {
+        // 🥅 Handle error
+        this.$http.onError((error) => {
+          if (error.statusCode == 401) {
+            this.err = true;
+          }
+        });
+        // 🚀 Submit login infos
+        const { token, user } = await this.$http.$post("/auth/login", {
+          name: this.login.toLowerCase(),
+          password: this.password,
+        });
+        // 🗃️ Store token & user datas
+        this.$store.commit("auth/setToken", token);
+        this.$store.commit("auth/setUser", user);
+      } catch (error) {
+        console.log(error);
+      } finally {
+      }
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 .container {
   @apply min-h-screen;
   @apply flex items-center;
 }
 section {
-  @apply flex-grow;
-}
-.aa {
   height: 36vw;
-  @apply flex flex-col items-center justify-between;
 }
 
 .title {
